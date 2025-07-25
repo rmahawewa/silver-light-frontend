8ab24af5e7d0f49ddafa5f442e74d37e769258ec
rmahawewa <rmahawewa@yahoo.com>
2025-07-25 21:29:29 +0530

    chat feature between two people

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -41,7 +41,73 @@ const Chat = () => {
 		fetchChatMessages();
 	}, []);
 
-	return <div></div>;
+	useEffect(() => {
+		const socket = createSocketConnection();
+		//As soon as the page loaded, the socket connection is made and joinChat event is emitted
+		socket.emit("joinChat", {
+			firstName: user.firstName,
+			userId,
+			targetUserId,
+		});
+
+		socket.on("messageReceived", ({ senderId, firstName, lastName, text }) => {
+			setMessages((messages) => [
+				...messages,
+				{ senderId, firstName, lastName, text },
+			]);
+		});
+
+		return () => {
+			socket.disconnect();
+		};
+	}, [userId, targetUserId]);
+
+	const sendMessage = () => {
+		const socket = createSocketConnection();
+		socket.emit("sendMessage", {
+			firstName: user.firstName,
+			lastName: user.lastName,
+			userId,
+			targetUserId,
+			text: newMessage,
+		});
+		setNewMessage("");
+	};
+
+	return (
+		<div className="w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col rounded">
+			<div className="p-5 border-b border-gray-600 rounded">
+				<h1>Chat</h1>
+				<h2>{targetUserName}</h2>
+			</div>
+			<div className="flex-1 overflow-scroll p-5 rounded">
+				{messages.map((message, index) => {
+					let direction =
+						message.senderId === user._id ? "chat-end" : "chat-start";
+					return (
+						<div key={index} className={"chat " + direction}>
+							<div className="chat-header">
+								{message.firstName} {message.lastName}
+								<time className="text-xs opacity-50">{message.createdAt}</time>
+							</div>
+							<div className="chat-bubble">{message.text}</div>
+							{/* <div className="chat-footer opacity-50">Seen</div> */}
+						</div>
+					);
+				})}
+			</div>
+			<div className="p-5 border-t border-gray-600 flex items-center gap-2 rounded">
+				<input
+					value={newMessage}
+					onChange={(e) => setNewMessage(e.target.value)}
+					className="flex-1 border border-gray-500 text-black rounded p-2"
+				/>
+				<button onClick={sendMessage} className="btn btn-primary">
+					Send
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Chat;
